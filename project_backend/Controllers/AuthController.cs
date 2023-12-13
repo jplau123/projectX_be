af194f64df4632a036d8502f2a5cb02b0cbe2414
jplau123 <j.plausinaitis@adform.com>
2023-12-13 14:23:09 +0200

    Added Auth Controller

---
+++ project_backend/Controllers/AuthController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace project_backend.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+
+}
