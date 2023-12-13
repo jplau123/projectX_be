8d0bca4ac2e8d6e09bad639aa3a41307c4c458a0
Erikas Šilobritas <e.silobritas@adform.com>
2023-12-13 17:12:29 +0200

    added dbup and npgsql

--- project_backend/Program.cs
+++ project_backend/Program.cs
@@ -1,4 +1,13 @@
+
+
+using DbUp;
+using Npgsql;
+using System.Data;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("PostgreConnection");
+
 
 // Add services to the container.
 
@@ -6,6 +15,32 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+EnsureDatabase.For.PostgresqlDatabase(connectionString);
+
+
+
+var upgrader =
+    DeployChanges.To
+        .PostgresqlDatabase(connectionString)
+        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+        .LogToConsole()
+        .Build();
+
+var result = upgrader.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+#if DEBUG
+    Console.ReadLine();
+#endif
+}
+
+
+builder.Services.AddTransient<IDbConnection>(sp => new NpgsqlConnection(connectionString));
 
 var app = builder.Build();
 
