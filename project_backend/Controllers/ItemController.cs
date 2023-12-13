17fb9f8a258b758cac438a970695bbad46de8ca0
Gabrielė Barsukova <g.barsukova@adform.com>
2023-12-13 19:50:34 +0200

    item repository, service, class, dtos, controller (get)

---
+++ project_backend/Controllers/ItemController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project_backend.Interfaces;
+using Serilog;
+
+namespace project_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ItemController : Controller
+    {
+        private readonly IItemService _itemService;
+        [HttpGet]
+        public ActionResult GetItems()
+        {
+            try
+            {
+                var itemsList = _itemService.GetItems();
+
+                if (itemsList == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(itemsList);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occured: {ErrorMessage}", ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
