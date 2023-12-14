14a0d4ea0db5ee194de93e1e88bf38fae10b204d
Erikas Šilobritas <e.silobritas@adform.com>
2023-12-14 12:16:48 +0200

    top up user balance, needs await

---
+++ project_backend/Model/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿namespace project_backend.Model.Entities
+{
+    public class User
+    {
+        public int User_Id { get; set; }
+
+        public string User_Name { get; set; }
+
+        public int Balance { get; set; }
+
+        public string Role { get; set; }
+
+        public bool Active { get; set; }
+
+        public DateTime Created_At { get; set; }
+
+        public string Created_By { get; set; }
+
+        public DateTime Modified_At { get; set; }
+
+        public string Modified_By { get; set; }
+
+    }
+}
---
+++ project_backend/Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using project_backend.Interfaces;
+using System.Data;
+
+namespace project_backend.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public UserRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public int AddUserBalance(int user_id, int balance)
+        {
+            string sql = $"UPDATE users SET balance = @Balance WHERE user_id = @User_Id RETURNING balance";
+            var queryArguments = new
+            {
+                Balance = balance,
+                User_Id = user_id
+            };
+
+
+            int newBalance = _connection.QuerySingleOrDefault<int>(sql, queryArguments, null);
+            return newBalance;
+
+        }
+    }
+}
