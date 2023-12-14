9edfa847cd95ec74d493e5cc648f11a73cdc60e3
Gabriele <152408732+Gabriele-ele@users.noreply.github.com>
2023-12-14 12:38:14 +0200

    Update ItemDTO.cs

--- project_backend/DTOs/ItemDTO.cs
+++ project_backend/DTOs/ItemDTO.cs
@@ -7,7 +7,7 @@
         public decimal Price { get; set; }
         public int Amount { get; set; }
         public DateTime Created_At { get; set; }
-        public string Created_By { get; set; }
+        public string ? Created_By { get; set; }
         public DateTime ? Modified_At { get; set; }
         public string ? Modified_By { get; set; }
     }
